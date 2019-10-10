326651190f86377537809a1e323f5a44a936ad06
Ashby Creighton Crum <acreighton1@dmacc.edu>
2019-10-09 21:07:45 -0700

    Commit assignment 5.1 and 6.1

---
+++ CIS174_TestCoreApp-master/Assignment_5_1_StarterCode/CIS174_TestCoreApp/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CIS174_TestCoreApp.Models;
+
+namespace CIS174_TestCoreApp.Controllers
+{
+   
+    public class StudentController: Controller
+    {
+        public IActionResult Index([FromRoute]int id)
+        {
+            var studentsList = new List<StudentViewModel>
+            {
+                new StudentViewModel {FirstName ="Billy", LastName = "Bob", Grade = 73},
+                new StudentViewModel {FirstName ="Bob", LastName = "Bill", Grade = 69},
+                new StudentViewModel {FirstName ="Flippy", LastName = "Flop", Grade = 82},
+                new StudentViewModel {FirstName ="Tim", LastName = "Nerd", Grade = 94}
+            };
+
+            var viewModel = new ViewModel
+                            {
+                                AccessLevel = id,
+                                Students = studentsList
+                            };
+            
+            return View(viewModel);
+        }
+    }
+}
---
+++ CIS174_TestCoreApp-master/Assignment_5_1_StarterCode/CIS174_TestCoreApp/Models/CreatePersonViewModel.cs
@@ -0,0 +1,46 @@
+﻿using DataAnnotationsExtensions;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using UrlAttribute = System.ComponentModel.DataAnnotations.UrlAttribute;
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class CreatePersonViewModel
+    {
+        [Required]
+        [StringLength(25)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(25)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Min(1)]
+        [Max(120)]
+        public int Age { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare("Email")]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+        [Url]
+        [MinLength(7)]
+        public string Website { get; set; }
+
+        [ReadOnly(true)]
+        [DisplayName("DMACC")]
+        public string School { get; set; }
+    }
+}
